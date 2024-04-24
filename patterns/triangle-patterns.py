8bf4dc72b46a209b4158b13a9d12a77f119fa999
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-04-24 14:39:20 +0200

    Update triangle-patterns.py

--- patterns/triangle-patterns.py
+++ patterns/triangle-patterns.py
@@ -1,5 +1,7 @@
-# triangle with numbers
-
+# # triangle with numbers
+# ''' 1
+#     1 2
+#     1 2 3 '''
 # n = int(input())
 #
 # for i in range(1, n + 1):
@@ -7,10 +9,23 @@
 #         print(j, end=" ")
 #     print()
 #
+#
+# # triangle with stars
+#
+# n = int(input())
+#
+# for i in range(1, n + 1):
+#     print('* ' * i)
 
-# triangle with stars
 
-n = int(input())
-
-for i in range(1, n + 1):
-    print('* ' * i)
+# # triangle with nums
+# ''' 1
+#     2 2
+#     3 3 3 '''
+#
+# n = 5
+#
+# for i in range(1, n + 1):
+#     for j in range(1, i + 1):
+#         print(i, end=" ")
+#     print()
