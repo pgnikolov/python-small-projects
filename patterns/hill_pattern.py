7af9f19d009e91d6b4b9b34eec52401ead603c95
pgnikolov <151896883+pgnikolov@users.noreply.github.com>
2024-05-03 11:25:48 +0200

    Create hill_pattern.py

---
+++ patterns/hill_pattern.py
@@ -0,0 +1,10 @@
+number = 5
+
+for i in range(number):
+    for j in range(i, number):
+        print(" ", end=" ")
+    for j in range(i):
+        print("*", end=" ")
+    for j in range(i + 1):
+        print("*", end=" ")
+    print()
