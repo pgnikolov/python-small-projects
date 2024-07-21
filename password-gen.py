08aab8fcb90a81648c4479f65b297e4f55fa9604
plamen <pgnikolov@gmail.com>
2024-07-21 17:39:33 +0200

    updated with use of string library

--- password-gen.py
+++ password-gen.py
@@ -1,33 +1,14 @@
 import random
+import string
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-           'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-           'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+chars = string.ascii_letters + string.digits + string.punctuation
 
 print("Password Generator!")
 
-letters_amount = int(input("How many letters you want in your password?\n"))
-symbols_amount = int(input(f"How many symbols you want?\n"))
-digit_amount = int(input(f"How many numbers you want?\n"))
+password_length = int(input("Enter password length: "))
 password_list = []
 
-for char in range(1, letters_amount + 1):
-    password_list.append(random.choice(letters))
+for char in range(1, password_length + 1):
+    password_list.append(random.choice(chars))
 
-for char in range(1, symbols_amount + 1):
-    password_list += random.choice(symbols)
-
-for char in range(1, digit_amount + 1):
-    password_list += random.choice(numbers)
-
-print(password_list)
-random.shuffle(password_list)
-print(password_list)
-
-password = ""
-for char in password_list:
-    password += char
-
-print(f"Your password is: {password}")
+print(f"Your password is: {''.join(password_list)}")
